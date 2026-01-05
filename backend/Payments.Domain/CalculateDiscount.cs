b94d8bac79266e94bb55cd4e71744fb1245643c3
GhatageManish <ghatagemanish02@gmail.com>
2026-01-05 17:05:01 +0530

    Fix final amount calculation in discount logic

--- backend/Payments.Domain/CalculateDiscount.cs
+++ backend/Payments.Domain/CalculateDiscount.cs
@@ -6,13 +6,32 @@ namespace Payments.Domain
     {
         public static CardType GetCardType(string cardNumber)
         {
+            if(string.IsNullOrWhiteSpace(cardNumber))
+                return CardType.RuPay;
+            if (cardNumber.StartsWith('4'))
+                return CardType.Visa;
+            if(cardNumber.StartsWith('5'))
+                return CardType.MasterCard;
+            if(cardNumber.StartsWith('6'))
+                return CardType.RuPay;
             return CardType.RuPay;
         }
 
         public static (bool DiscountApplied, decimal FinalAmount) Calculate(CardType cardType, decimal amount)
         {
+            decimal discountPercentage = cardType switch
+            {
+                 CardType.Visa => 0m,
+                 CardType.MasterCard => 0.05m,
+                 CardType.RuPay => 0.10m,
+                 _ => 0.10m
+            };
 
-            return (true, 0);
+            if (discountPercentage == 0)
+                return (false, amount);
+
+            var finalAmount = amount - (amount * discountPercentage);
+            return (true, finalAmount);
         }
     }
 }
